482b610c7076f6eceec7cbbabd2a6942ae8da4d0
trinidad echevarria <trinidadechevarria_@hotmail.com>
2022-07-20 20:21:03 -0300

    subiendo a git

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express'); 
+
+const server = express(); 
+
+const port = process.env.PORT || 2020;
+
+const start = () => console.log('Starting server'); 
+
+server.listen(port, start()); 
+
+const path = require('path'); 
+
+const public = path.join(__dirname, '/public'); 
+
+const statics = express.static(public) 
+
+server.use(statics);
+
+server.get('/', function(req, res){
+    let file = path.join(__dirname, 'views', 'home.html');
+    res.sendFile(file);
+})
+
+server.get('/login', function(req, res){
+    let file = path.join(__dirname, 'views', 'login.html');
+    res.sendFile(file);
+})
+
+server.get('/register', function(req, res){
+    let file = path.join(__dirname, 'views', 'register.html');
+    res.sendFile(file);
+})
